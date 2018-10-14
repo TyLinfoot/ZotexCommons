21fe317445c79f09e3a04e2295a8cda0a65696c6
ZotexMC <wtid2003@gmail.com>
2018-10-14 16:38:34 +0100

    Edited command

--- src/net/zotex/zotexcommons/commands/NightvisionCommand.java
+++ src/net/zotex/zotexcommons/commands/NightvisionCommand.java
@@ -1,7 +1,30 @@
 package net.zotex.zotexcommons.commands;
 
+import net.zotex.zotexcommons.ZotexCommons;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
 public class NightvisionCommand {
 
+    private ZotexCommons plugin;
+    public NightvisionCommand(ZotexCommons plugin) {
+        this.plugin = plugin;
+    }
 
+    public ArrayList<String> nightvision = new ArrayList();
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if(!(sender instanceof Player)) {
+            return true;
+        }
+        Player p = (Player)sender;
+        if(!p.hasPermission(""))
+
+        return false;
+    }
 
 }
