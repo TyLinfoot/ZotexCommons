08c1e9104648b8dd5233118b23ba74071baf5f15
ItsAlfie <alfielee645@gmail.com>
2018-10-15 18:27:36 +0100

    Added gamemode command with arguments creative (c), survival (s) and spectator (sp)

--- src/net/zotex/zotexcommons/ZotexCommons.java
+++ src/net/zotex/zotexcommons/ZotexCommons.java
@@ -2,6 +2,7 @@ package net.zotex.zotexcommons;
 
 import net.zotex.zotexcommons.commands.HelpCommand;
 import net.zotex.zotexcommons.commands.NightvisionCommand;
+import net.zotex.zotexcommons.commands.GamemodeCommand;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,6 +17,7 @@ public class ZotexCommons extends JavaPlugin implements Listener {
 
         getCommand("help").setExecutor(new HelpCommand(this));
         getCommand("nightvision").setExecutor((CommandExecutor) new NightvisionCommand(this));
+        getCommand("gamemode").setExecutor(new GamemodeCommand(this);
 
     }
 }
