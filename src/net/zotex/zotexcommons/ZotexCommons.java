160c872d83b81a2a3f34c15bfbcc22fdcb5b2f83
ZotexMC <wtid2003@gmail.com>
2018-10-16 15:46:57 +0100

    Added "Utils" Package, as well as adding the ability to change messages via config.

--- src/net/zotex/zotexcommons/ZotexCommons.java
+++ src/net/zotex/zotexcommons/ZotexCommons.java
@@ -1,5 +1,7 @@
 package net.zotex.zotexcommons;
 
+import net.zotex.zotexcommons.commands.FlyingCommand;
+import net.zotex.zotexcommons.commands.GamemodeCommand;
 import net.zotex.zotexcommons.commands.HelpCommand;
 import net.zotex.zotexcommons.commands.NightvisionCommand;
 import org.bukkit.command.CommandExecutor;
@@ -8,14 +10,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ZotexCommons extends JavaPlugin implements Listener {
 
+    private static ZotexCommons instance;
+
     @Override
     public void onEnable() {
+
+        setInstance(this);
         System.out.println("The ZotexCommons plugin has been loaded.");
 
         saveDefaultConfig();
 
         getCommand("help").setExecutor(new HelpCommand(this));
         getCommand("nightvision").setExecutor((CommandExecutor) new NightvisionCommand(this));
+        getCommand("gamemode").setExecutor(new GamemodeCommand(this));
+        getCommand("flying").setExecutor(new FlyingCommand(this));
+    }
 
+    public static ZotexCommons getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(ZotexCommons instance) {
+        instance = instance;
     }
 }
